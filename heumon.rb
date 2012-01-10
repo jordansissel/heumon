e42420f556b6cf0dacfa4d8fff948ba5a88d46bf
Jordan Sissel <jls@semicomplete.com>
2012-01-10 01:29:52 -0800

    - closer to a working prototype

--- heumon.rb
+++ heumon.rb
@@ -1,5 +1,10 @@
 require "sinatra"
 
+# TODO(sissel): make this for iPhone/android or whatever.
+before :agent => /.*/ do
+  @layout = :"layout-mobile"
+end
+
 get "/" do
-  haml :index
+  haml :index, :layout => (@layout or :layout)
 end
